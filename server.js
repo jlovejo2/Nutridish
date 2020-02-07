6871c16b8507fd96a33ca1a90e726c878e2c94ee
James Lovejoy <james.lovejoy2@gmail.com>
2020-02-06 21:39:21 -0600

    added get route that performs a call to externa api

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const db = require('./models');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-db.sequelize.sync({force: true}).then(function() {
+
+//Routes
+// ==============================
+
+require('./routes/nutrients-routes.js')(app);
+
+//Syncing our sequelize models created in model folder.
+// This line of code also starts our express app
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("Listening on port %s", PORT)
     });
 });
+
+// {force: true}
