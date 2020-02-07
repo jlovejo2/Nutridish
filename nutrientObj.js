0b2d7cbef4a23ab38c35fc3783c3efeb7f9ee5cc
James Lovejoy <james.lovejoy2@gmail.com>
2020-02-06 20:06:44 -0600

    corrected naming in nutrientObj used for seeding

--- nutrientObj.js
+++ nutrientObj.js
@@ -1,13 +1,12 @@
 const functions = require('./functions');
-const nutrientNameArr = ['CA', 'CHOCDF', 'CHOLE', 'FAMS', 'FAPU', 'FASAT', 'FAT', 'FATRN', 'FE', 'FIBTG', 'FOLDFE', 'K', 'MG', 'NA', 'NIA', 'P', 'PROCNT', 'RIBF', 'SUGAR', 'THIA', 'TOCPHA', 'VITA_RAE', 'VITB12', 'VITB6A', 'VITC', 'VITD', 'VITK1', 'ZN']
-const nutrientCodeArr = ['calcium', 'carbs', 'cholesterol', 'monounsaturated', 'polyunsatured', 'saturated', 'fat', 'trans', 'iron', 'fiber', 'folate', 'potassium', 'magnesium', 'sodium', 'energy', 'niacin', 'phosphorus', 'protein', 'riboflavin', 'sugars', 'thiamin', 'vitamin E', 'vitamin A', 'vitamin B12', 'vitamin B6', 'viatmin C', 'vitamin D', 'Viatmin K']
-const nutrientUnitArr = []
-const nutrientKeysArr = ["name", "code"]
+const nutrientCodeArr = ['CA', 'CHOCDF', 'CHOLE', 'FAMS', 'FAPU', 'FASAT', 'FAT', 'FATRN', 'FE', 'FIBTG', 'FOLDFE', 'K', 'MG', 'NA', 'NIA', 'P', 'PROCNT', 'RIBF', 'SUGAR', 'THIA', 'TOCPHA', 'VITA_RAE', 'VITB12', 'VITB6A', 'VITC', 'VITD', 'VITK1', 'ZN'];
+const nutrientNameArr = ['calcium', 'carbs', 'cholesterol', 'monounsaturated', 'polyunsatured', 'saturated', 'fat', 'trans', 'iron', 'fiber', 'folate', 'potassium', 'magnesium', 'sodium', 'energy', 'niacin', 'phosphorus', 'protein', 'riboflavin', 'sugars', 'thiamin', 'vitamin E', 'vitamin A', 'vitamin B12', 'vitamin B6', 'viatmin C', 'vitamin D', 'Viatmin K'];
+const nutrientUnitArr = [];
+const nutrientKeysArr = ["nutrientName", "nutrientApiCode"];
 
-
+//This line of code calls the createObj() function in the functions.js file.  This function will create an object with the based on the values of the two
+//arrays   
 const nutrientsArr = functions.createObj(nutrientNameArr, nutrientCodeArr, nutrientKeysArr)
-
-
 
 
 module.exports = nutrientsArr;
