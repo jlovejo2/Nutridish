633b12f549dbfa155e4a5b954b0085cac834127a
Joe Shields <shields.john.joseph@gmail.com>
2020-02-14 22:52:22 -0600

    added seeds

---
+++ db/protein_seeds.sql
@@ -0,0 +1,9 @@
+USE nutridish_db;
+
+INSERT INTO proteins (proteinName) VALUES ('Beef');
+INSERT INTO proteins (proteinName) VALUES ('Chicken');
+INSERT INTO proteins (proteinName) VALUES ('Lamb');
+INSERT INTO proteins (proteinName) VALUES ('Pork');
+INSERT INTO proteins (proteinName) VALUES ('Seitan');
+INSERT INTO proteins (proteinName) VALUES ('Steak');
+INSERT INTO proteins (proteinName) VALUES ('Tofu');
