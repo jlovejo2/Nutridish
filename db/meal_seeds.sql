00e2bfe80500df39e187bb294d700a1abc76eddd
Joe Shields <shields.john.joseph@gmail.com>
2020-02-15 03:11:02 -0600

    SQL seeding format change

---
+++ db/meal_seeds.sql
@@ -0,0 +1,6 @@
+USE kiwbxw4e3dt7q258;
+
+INSERT INTO mealType (mealTypeApiCode) VALUES ('Breakfast');
+INSERT INTO mealType (mealTypeApiCode) VALUES ('Lunch');
+INSERT INTO mealType (mealTypeApiCode) VALUES ('Dinner');
+INSERT INTO mealType (mealTypeApiCode) VALUES ('Snack');
--- db/protein_seeds.sql
+++ db/protein_seeds.sql
@@ -1,9 +1,9 @@
 USE kiwbxw4e3dt7q258;
 
-INSERT INTO protein (proteinApiCode) VALUES ('Beef');
-INSERT INTO protein (proteinApiCode) VALUES ('Chicken');
-INSERT INTO protein (proteinApiCode) VALUES ('Lamb');
-INSERT INTO protein (proteinApiCode) VALUES ('Pork');
-INSERT INTO protein (proteinApiCode) VALUES ('Seitan');
-INSERT INTO protein (proteinApiCode) VALUES ('Steak');
-INSERT INTO protein (proteinApiCode) VALUES ('Tofu');
+INSERT INTO proteins (proteinApiCode) VALUES ('Beef');
+INSERT INTO proteins (proteinApiCode) VALUES ('Chicken');
+INSERT INTO proteins (proteinApiCode) VALUES ('Lamb');
+INSERT INTO proteins (proteinApiCode) VALUES ('Pork');
+INSERT INTO proteins (proteinApiCode) VALUES ('Seitan');
+INSERT INTO proteins (proteinApiCode) VALUES ('Steak');
+INSERT INTO proteins (proteinApiCode) VALUES ('Tofu');
