6312e0b058b92ac95578733c7543b2493f265e24
Joe Shields <shields.john.joseph@gmail.com>
2020-02-15 03:11:58 -0600

    added mealTypes to routes

--- routes/nutrients-routes.js
+++ routes/nutrients-routes.js
@@ -31,16 +31,23 @@ router.get('/api/proteinCodes', function (req, res) {
     res.json(dbProtein);
   });
 });
+//This route will return all the mealType options available in the mealType table
+router.get('/api/mealTypeCodes', function (req, res) {
 
+  db.MealType.findAll({ attributes: ['mealTypeApiCode'] }).then(function (dbMealType) {
+    console.log('inside get response');
+    res.json(dbMealType);
+  });
+});
 //This route is the biggest aspect of our app and alot is happening in it.
 //Esesentially when the user performs a search we check if the search exist in our database.  If it doesn't we got out to the external api 
 //and bring back the result, create associations between those recipes and that search, and render the recipes to the page.
 //If a search is found in our database then we pull them from our database and render them to the page. 
 
-router.get('/api/nutrients/:userEmail/:nutrients/:healthCode/:proteinCode', function (req, res) {
+router.get('/api/nutrients/:userEmail/:nutrients/:healthCode/:proteinCode/:mealTypeCode', function (req, res) {
 
   console.log(
-    req.params.userEmail + 'has searched for ' + req.params.nutrients + ' & ' + req.params.healthCode + ' & ' + req.params.proteinCode
+    req.params.userEmail + 'has searched for ' + req.params.nutrients + ' & ' + req.params.healthCode + ' & ' + req.params.proteinCode + ' & ' +req.params.mealTypeCode
   );
 
   // const userEmail = req.params.userEmail;
@@ -63,7 +70,8 @@ router.get('/api/nutrients/:userEmail/:nutrients/:healthCode/:proteinCode', func
         searchQuery: queryURL,
         NutrientCode: req.params.nutrients,
         HealthApiCode: req.params.healthCode,
-        ProteinApiCode: req.params.proteinCode
+        ProteinApiCode: req.params.proteinCode,
+        mealTypeApiCode: req.params.mealTypeCode,
       }).then(function (newSearch) {
         console.log('past search create');
 
@@ -227,4 +235,3 @@ router.get('/searches/:userEmail', function (req /*, res*/) {
 
 //This is to return all recipes for the search
 module.exports = router;
-
